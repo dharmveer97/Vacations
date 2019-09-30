bac8ef9e8ffb60dce792c9ea25cb6eace4fd64ad
dvbangar97 <dvbangar8@gmail.com>
2019-09-30 13:15:09 +0530

    minor changes

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Section = styled.div`
+  background: #f1f1f1;
+`;
 
 const Destination = () => (
-  <section className="section">
+  <Section className="section">
     <div className="container">
       <div className="columns">
         <div className="column is-half">
@@ -9,7 +14,7 @@ const Destination = () => (
             <div className="column is-half">
               <div className="media">
                 <div className="media-content">
-                  <span className="icon is-large is-size-1">
+                  <span className="icon is-large is-size-1 has-text-warning">
                     <i className="fas fa-walking" />
                   </span>
                   <p className="title is-4">Activities</p>
@@ -23,8 +28,8 @@ const Destination = () => (
             <div className="column is-half">
               <div className="media">
                 <div className="media-content">
-                  <span className="icon is-large is-size-1">
-                    <i className="fas fa-home" />
+                  <span className="icon is-large is-size-1 has-text-warning">
+                    <i className="fas fa-route" />
                   </span>
                   <p className="title is-4">Travel Arrangements</p>
                   <p className="subtitle is-6">
@@ -37,7 +42,7 @@ const Destination = () => (
             <div className="column is-half">
               <div className="media">
                 <div className="media-content">
-                  <span className="icon is-large is-size-1">
+                  <span className="icon is-large is-size-1 has-text-warning">
                     <i className="fas fa-home" />
                   </span>
                   <p className="title is-4">Private Guide</p>
@@ -51,7 +56,7 @@ const Destination = () => (
             <div className="column is-half">
               <div className="media">
                 <div className="media-content">
-                  <span className="icon is-large is-size-1">
+                  <span className="icon is-large is-size-1 has-text-warning">
                     <i className="fas fa-home" />
                   </span>
                   <p className="title is-4">Location Manager</p>
@@ -65,29 +70,26 @@ const Destination = () => (
           </div>
         </div>
         <div className="column is-half">
-          <section className="hero">
-            <div className="hero-body">
-              <div className="container">
-                <h1 className="title is-1">
-                  It s time to start your adventure
-                </h1>
-                <p className="subtitle">
-                  A small river named Duden flows by their place and supplies it
-                  with the necessary regelialia. It is a paradisematic country,
-                  in which roasted parts of sentences fly into your mouth.
-                </p>
-                <p className="subtitle">
-                  A small river named Duden flows by their place and supplies it
-                  with the necessary regelialia. It is a paradisematic country,
-                  in which roasted parts of sentences fly into your mouth.
-                </p>
-              </div>
+          <div className="">
+            <div className="container">
+              <h1 className="title is-1">It s time to start your adventure</h1>
+              <p className="subtitle">
+                A small river named Duden flows by their place and supplies it
+                with the necessary regelialia. It is a paradisematic country, in
+                which roasted parts of sentences fly into your mouth.
+              </p>
+              <p className="subtitle">
+                A small river named Duden flows by their place and supplies it
+                with the necessary regelialia. It is a paradisematic country, in
+                which roasted parts of sentences fly into your mouth.
+              </p>
+              <a className="button is-warning is-rounded">Search Destination</a>
             </div>
-          </section>
+          </div>
         </div>
       </div>
     </div>
-  </section>
+  </Section>
 );
 
 export default Destination;
