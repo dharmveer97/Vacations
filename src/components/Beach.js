489dd01d44c45170aa55b823d5a5a776e349dd64
dvbangar97 <dvbangar8@gmail.com>
2019-10-05 11:08:54 +0530

    add new components

--- src/components/Beach.js
+++ src/components/Beach.js
@@ -1,27 +1,70 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Beach = props => (
-  <div className="card">
-    <div className="card-image">
-      <figure className="image is-4by3">
-        <img src={props.img} />
-      </figure>
-    </div>
-    <div className="card-content">
-      <div className="media">
-        <div className="media-content">
-          <p className="title is-4">John Smith</p>
-          <p className="subtitle is-6">@johnsmith</p>
+const Wrapper = styled.div`
+  img {
+    object-fit: cover;
+  }
+  .uppers {
+    margin-top: -2.8rem;
+  }
+`;
+
+const Beach = props => {
+  const {
+    img,
+    title,
+    subtitle,
+    button,
+    icontext,
+    icon,
+    home,
+    king,
+    mountain,
+  } = props;
+
+  return (
+    <Wrapper>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3 fit">
+            <img src={img} alt="all-pels" />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <div className="has-text-centered">
+                <a className="button is-primary is-rounded uppers">{button}</a>
+              </div>
+              <p className="title is-6 is-size-6-mobile">{title}</p>
+              <p className="subtitle is-4 is-size-6-mobile">{subtitle}</p>
+              <p className="location">
+                <span className="icon is-left">
+                  <i className={icon} />
+                </span>
+                {icontext}
+              </p>
+            </div>
+          </div>
+          <div className="content">
+            <span className="icon has-text-info">
+              <i className={home} />
+            </span>
+            2
+            <span className="icon has-text-success">
+              <i className={king} />
+            </span>
+            3
+            <span className="icon has-text-warning">
+              <i className={mountain} />
+            </span>
+            Near Mountain
+          </div>
         </div>
       </div>
+    </Wrapper>
+  );
+};
 
-      <div className="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-        iaculis mauris. <a>@bulmaio</a>.<a href="#">#css</a>{' '}
-        <a href="#">#responsive</a>
-        <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-      </div>
-    </div>
-  </div>
-);
 export default Beach;
--- src/components/BeachContent.js
+++ src/components/BeachContent.js
@@ -4,24 +4,84 @@ import Beach from './Beach';
 const BeachContent = () => (
   <section className="section">
     <div className="container">
-    <h1 className="title is-1 has-text-centered">Best Place Destination</h1>
-      <div className="columns">
-        <div className="column">
+      <h1 className="title is-1 has-text-centered">Best Place Destination</h1>
+      <div className="columns is-multiline is-mobile">
+        <div className="column is-one-third">
           <Beach
-            img="https://colorlib.com/preview/theme/vacation/images/place-1.jpg"
-            title="Singapore"
+            img="https://colorlib.com/preview/theme/vacation/images/destination-1.jpg"
+            title="8 Days Tour"
+            subtitle="Bali, Indonesia"
+            button="$300/person"
+            icontext="Bali, Indonesia"
+            icon="fas fa-map"
+            home="fas fa-bath"
+            king="fas fa-chess-king"
+            mountain="fas fa-tram"
           />
         </div>
-        <div className="column">
+        <div className="column is-one-third">
           <Beach
-            img="https://colorlib.com/preview/theme/vacation/images/place-2.jpg"
-            title="Canada"
+            img="https://colorlib.com/preview/theme/vacation/images/destination-2.jpg"
+            title="8 Days Tour"
+            subtitle="Bali, Indonesia"
+            button="$300/person"
+            icontext="Bali, Indonesia"
+            icon="fas fa-map"
+            home="fas fa-bath"
+            king="fas fa-chess-king"
+            mountain="fas fa-tram"
           />
         </div>
-        <div className="column">
+        <div className="column is-one-third">
           <Beach
-            img="https://colorlib.com/preview/theme/vacation/images/place-3.jpg"
-            title="Thailand"
+            img="https://colorlib.com/preview/theme/vacation/images/destination-3.jpg"
+            title="8 Days Tour"
+            subtitle="Bali, Indonesia"
+            button="$300/person"
+            icontext="Bali, Indonesia"
+            icon="fas fa-map"
+            home="fas fa-bath"
+            king="fas fa-chess-king"
+            mountain="fas fa-tram"
+          />
+        </div>
+        <div className="column is-one-third">
+          <Beach
+            img="https://colorlib.com/preview/theme/vacation/images/destination-4.jpg"
+            title="8 Days Tour"
+            subtitle="Bali, Indonesia"
+            button="$300/person"
+            icontext="Bali, Indonesia"
+            icon="fas fa-map"
+            home="fas fa-bath"
+            king="fas fa-chess-king"
+            mountain="fas fa-tram"
+          />
+        </div>
+        <div className="column is-one-third">
+          <Beach
+            img="https://colorlib.com/preview/theme/vacation/images/destination-5.jpg"
+            title="8 Days Tour"
+            subtitle="Bali, Indonesia"
+            button="$300/person"
+            icontext="Bali, Indonesia"
+            icon="fas fa-map"
+            home="fas fa-bath"
+            king="fas fa-chess-king"
+            mountain="fas fa-tram"
+          />
+        </div>
+        <div className="column is-one-third">
+          <Beach
+            img="https://colorlib.com/preview/theme/vacation/images/destination-6.jpg"
+            title="8 Days Tour"
+            subtitle="Bali, Indonesia"
+            button="$300/person"
+            icontext="Bali, Indonesia"
+            icon="fas fa-map"
+            home="fas fa-bath"
+            king="fas fa-chess-king"
+            mountain="fas fa-tram"
           />
         </div>
       </div>
